1d20bce108cf0219edec3e5a0f0d20d4147aa349
teafr <tookyoumy7@gmail.com>
2025-03-06 19:05:58 +0200

    created ReviewService to deal with three repository. Made crud operations and pulled repeated code to private methods

---
+++ BooksApp/Services/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using booksAPI.Models.DatabaseModels;
+
+namespace web_api_for_books_app.Services
+{
+    public interface IReviewService
+    {
+        Task<Review> CreateAsync(Review review);
+        Task DeleteAsync(Review review);
+        Task<List<Review>?> GetAsync();
+        Task<Review?> GetByIdAsync(int id);
+        Task UpdateAsync(Review review);
+    }
+}
---
+++ BooksApp/Services/ReviewService.cs
@@ -0,0 +1,75 @@
+﻿using booksAPI.Models.DatabaseModels;
+using booksAPI.Repositories;
+
+namespace web_api_for_books_app.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IRepository<Review> _reviewRepository;
+        private readonly IRepository<Book> _bookRepository;
+        private readonly IRepository<User> _userRepository;
+
+        public ReviewService(IRepository<Review> repository, IRepository<Book> book, IRepository<User> user)
+        {
+            this._reviewRepository = repository;
+            this._bookRepository = book;
+            this._userRepository = user;
+        }
+        public async Task<List<Review>?> GetAsync()
+        {
+            return await _reviewRepository.GetAsync();
+        }
+
+        public async Task<Review?> GetByIdAsync(int id)
+        {
+            return await _reviewRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Review> CreateAsync(Review review)
+        {
+            await InitializeObjectsOfReview(review);
+            return await _reviewRepository.CreateAsync(review);
+        }
+
+        public async Task UpdateAsync(Review review)
+        {
+            await InitializeObjectsOfReview(review);
+            await _reviewRepository.UpdateAsync(review);
+        }
+
+        public async Task DeleteAsync(Review review)
+        {
+            await _reviewRepository.DeleteAsync(review);
+        }
+
+        private async Task InitializeObjectsOfReview(Review review)
+        {
+            await InitializeUser(review);
+            await InitializeBook(review);
+        }
+
+        private async Task InitializeUser(Review review)
+        {
+            if (review.ReviwerId != 0)
+            {
+                User? user = await _userRepository.GetByIdAsync(review.ReviwerId);
+                if (user != null)
+                {
+                    review.User = user;
+                }
+            }
+        }
+
+        private async Task InitializeBook(Review review)
+        {
+            if (review.BookId != 0)
+            {
+                Book? book = await _bookRepository.GetByIdAsync(review.BookId);
+                if (book != null)
+                {
+                    review.Book = book;
+                }
+            }
+        }
+    }
+}
