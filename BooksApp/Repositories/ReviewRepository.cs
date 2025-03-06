f240ae78cec2fa96d0d1446cf31c72ba67b84b96
teafr <tookyoumy7@gmail.com>
2025-03-06 19:04:00 +0200

    overrided ReviewRepository methods to include user and book in review object and to attach already created user/book

--- BooksApp/Repositories/ReviewRepository.cs
+++ BooksApp/Repositories/ReviewRepository.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using booksAPI.Contexts;
+﻿using booksAPI.Contexts;
 using booksAPI.Models.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
 using web_api_for_books_app.Repositories;
 
 namespace booksAPI.Repositories
@@ -12,6 +12,44 @@ namespace booksAPI.Repositories
         public override async Task<List<Review>?> GetAsync()
         {
             return await dbSet.Include(user => user.User).Include(book => book.Book).ToListAsync();
+        }
+
+        public override async Task<Review?> GetByIdAsync(int id)
+        {
+            return await dbSet.Include(user => user.User).Include(book => book.Book).FirstOrDefaultAsync(item => item.Id == id);
+        }
+        public override Task<Review> CreateAsync(Review newItem)
+        {
+            TryToAttachObjects(newItem);
+            return base.CreateAsync(newItem);
+        }
+
+        public override Task UpdateAsync(Review item)
+        {
+            TryToAttachObjects(item);
+            return base.UpdateAsync(item);
+        }
+
+        private void TryToAttachObjects(Review item)
+        {
+            TryToAttachUser(item);
+            TryToAttachBook(item);
+        }
+
+        private void TryToAttachUser(Review item)
+        {
+            if (item.ReviwerId != 0)
+            {
+                _context.Attach(item.User);
+            }
+        }
+
+        private void TryToAttachBook(Review item)
+        {
+            if (item.BookId != 0)
+            {
+                _context.Attach(item.Book);
+            }
         }
     }
 }
