2f4d3289e2a0dd9757d9d486b1f9b1230fe273f9
teafr <tookyoumy7@gmail.com>
2025-03-09 00:23:03 +0200

    made interface for reviews service generic to use it with all services

---
+++ BooksApp/Services/ICrudService.cs
@@ -0,0 +1,13 @@
+﻿using booksAPI.Models.DatabaseModels;
+
+namespace web_api_for_books_app.Services
+{
+    public interface ICrudService<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>?> GetAsync();
+        Task<TEntity?> GetByIdAsync(int id);
+        Task<TEntity> CreateAsync(TEntity review);
+        Task DeleteAsync(TEntity review);
+        Task UpdateAsync(TEntity review);
+    }
+}
