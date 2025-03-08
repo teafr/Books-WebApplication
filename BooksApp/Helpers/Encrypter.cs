8164ba7d8f23ebdfe86235511259176b8d13b3e6
teafr <tookyoumy7@gmail.com>
2025-03-08 21:14:29 +0200

    deleted useless method and added encrypter for hashing user passwords

---
+++ BooksApp/Helpers/Encrypter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booksAPI.Helpers
+{
+    public static class Encrypter
+    {
+        public static string Encrypt(string input)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(input);
+            data = SHA256.HashData(data);
+            return Encoding.ASCII.GetString(data);
+        }
+    }
+}
