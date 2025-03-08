50083c2f6d4633cb55f6d7025d377a7401fa3043
teafr <tookyoumy7@gmail.com>
2025-03-09 00:22:04 +0200

    made migration to save password column

---
+++ BooksApp/Migrations/20250308221944_NewPasswordColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace booksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPasswordColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                table: "users",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password",
+                table: "users");
+        }
+    }
+}
