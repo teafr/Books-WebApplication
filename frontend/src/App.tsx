dade494356ea3c0d74668452e8711ee68b206098
teafr <tookyoumy7@gmail.com>
2025-07-29 21:00:21 +0300

    Made Frontend in VS and created component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>This is a simple React application.</p>
+        <p>Feel free to explore and modify the code!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/book/Book.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props { 
+    bookName: string,
+    author: string,
+    imageUrl: string
+};
+
+const Book = (props : Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Book
